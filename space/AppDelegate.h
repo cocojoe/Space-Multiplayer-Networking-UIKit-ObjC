a356021eee144914240f1834c31a5b899aa06937
Martin Walsh <martin.walsh@gmail.com>
2012-10-09 09:58:52 +0100

    Initial Commit

---
+++ space/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  space
+//
+//  Created by Martin Walsh on 09/10/2012.
+//  Copyright (c) 2012 Pedro LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ space/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  space
+//
+//  Created by Martin Walsh on 09/10/2012.
+//  Copyright (c) 2012 Pedro LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
