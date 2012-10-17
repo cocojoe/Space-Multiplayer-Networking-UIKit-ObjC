343f4090d3e1f98c40efa8a8c2249ffc689a9757
Martin Walsh <martin.walsh@gmail.com>
2012-10-17 15:57:00 +0100

    Command Hub Expansion
    Tab Bar Selection Hub
    Seperated Player / Hangar Views
    PlayerProfileView Added (Customise Style)
    Currency Displayed

---
+++ space/classes/PlayerProfileView.h
@@ -0,0 +1,13 @@
+//
+//  PlayerProfileView.h
+//  space
+//
+//  Created by Martin Walsh on 17/10/2012.
+//  Copyright (c) 2012 Pedro LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerProfileView : UIView
+
+@end
---
+++ space/classes/PlayerProfileView.m
@@ -0,0 +1,31 @@
+//
+//  PlayerProfileView.m
+//  space
+//
+//  Created by Martin Walsh on 17/10/2012.
+//  Copyright (c) 2012 Pedro LTD. All rights reserved.
+//
+
+#import "PlayerProfileView.h"
+
+@implementation PlayerProfileView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
